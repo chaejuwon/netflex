2c13658a4017b4d016f3d20b75b6b81e5f24f7d8
CJW <cjw0214@spectory.net>
2025-07-14 13:57:29 +0900

    1) 리액트 넷플릭스 반응형 수정(Hash Router 추가)

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 import { createGlobalStyle } from "styled-components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter } from "react-router-dom";
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -107,12 +108,14 @@ const root = ReactDOM.createRoot(
 
 const client = new QueryClient();
 root.render(
-  <RecoilRoot>
-    <QueryClientProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </QueryClientProvider>
-  </RecoilRoot>
+  <HashRouter>
+    <RecoilRoot>
+      <QueryClientProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
+  </HashRouter>
 );
